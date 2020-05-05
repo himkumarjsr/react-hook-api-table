71e6a6eb4e4e3bfb63cd1094077b012dbd76d54b
himkumarjsr <himkumarjsr@gmail.com>
2020-05-05 15:56:30 +0530

    Updates

---
+++ precache-manifest.3e74849b423d4d1b5ba160701e8fcbd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e301d1d9d0aef3885b2343c6ec21bc4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e4b48b7bf834b81f97f",
+    "url": "/static/css/2.0eb562f5.chunk.css"
+  },
+  {
+    "revision": "fd84c70c02af16980ceb",
+    "url": "/static/css/main.1ab9711e.chunk.css"
+  },
+  {
+    "revision": "5e4b48b7bf834b81f97f",
+    "url": "/static/js/2.515f69cb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.515f69cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd84c70c02af16980ceb",
+    "url": "/static/js/main.27a0c239.chunk.js"
+  },
+  {
+    "revision": "031b8524f7c2ea82c588",
+    "url": "/static/js/runtime-main.76b4324f.js"
+  }
+]);
